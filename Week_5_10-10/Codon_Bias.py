4dc810bd3920e65b6d01474a2a3afeb88eb445ab
njacobs627 <ntjacob@emory.edu>
2016-10-17 10:15:16 -0400

    New Script w/Dictionary

--- Week_5_10-10/Codon_Bias.py
+++ Week_5_10-10/Codon_Bias.py
@@ -29,6 +29,71 @@ def NumberToBase(Number):
 
 def Codon_Bias(Input,Output):
 	Gene=' ' #Dummy Starting Value, overwritten first
+	Codon_Code={
+	'AAA':0,
+	'AAC':1,
+	'AAG':2,
+	'AAT':3,
+	'ACA':4,
+	'ACC':5,
+	'ACG':6,
+	'ACT':7,
+	'AGA':8,
+	'AGC':9,
+	'AGG':10,
+	'AGT':11,
+	'ATA':12,
+	'ATC':13,
+	'ATG':14,
+	'ATT':15,
+	'CAA':16,
+	'CAC':17,
+	'CAG':18,
+	'CAT':19,
+	'CCA':20,
+	'CCC':21,
+	'CCG':22,
+	'CCT':23,
+	'CGA':24,
+	'CGC':25,
+	'CGG':26,
+	'CGT':27,
+	'CTA':28,
+	'CTC':29,
+	'CTG':30,
+	'CTT':31,
+	'GAA':32,
+	'GAC':33,
+	'GAG':34,
+	'GAT':35,
+	'GCA':36,
+	'GCC':37,
+	'GCG':38,
+	'GCT':39,
+	'GGA':40,
+	'GGC':41,
+	'GGG':42,
+	'GGT':43,
+	'GTA':44,
+	'GTC':45,
+	'GTG':46,
+	'GTT':47,
+	'TAA':48,
+	'TAC':49,
+	'TAG':50,
+	'TAT':51,
+	'TCA':52,
+	'TCC':53,
+	'TCG':54,
+	'TCT':55,
+	'TGA':56,
+	'TGC':57,
+	'TGG':58,
+	'TGT':59,
+	'TTA':60,
+	'TTC':61,
+	'TTG':62,
+	'TTT':63}
 	Codon_Count=[0 for x in range(64)]
 	Codon_Number=0
 
@@ -44,11 +109,11 @@ def Codon_Bias(Input,Output):
 
 			for i in range(Gene_Codons):
 				Codon=Gene[(i*3):(i*3+3)]
-				Codon_Count[StringToNumber(Codon)]+=1
+				Codon_Count[Codon_Code[Codon]]+=1
 
 	with open(Output,"w") as output:
 		for i in range(64):
-			output.write("%s %f" % (NumberToString(i,3), Codon_Count[i]*1.0/Codon_Number))
+			output.write("%s %1.4f\n" % (NumberToString(i,3), Codon_Count[i]*1.0/Codon_Number))
 			#print "%s %f" % (NumberToString(i,3), Codon_Count[i]*1.0/Codon_Number)   #Option to Print
 
 import argparse
